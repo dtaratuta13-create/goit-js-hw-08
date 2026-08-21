cedb96da94d8c2d3aeedcd67ea7554239bb03847
TaratutaDmytro <dtaratuta13@gmail.com>
2026-08-22 00:46:16 +0200

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -6,5 +6,23 @@ const images = [
       'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg',
     description: 'Hokkaido Flower',
   },
-
 ];
+
+const gallery = document.querySelector('.gallery');
+
+const galleryItem = document.createElement('li');
+
+galleryItem.classList.add('gallery-item');
+
+galleryItem.innerHTML = `
+  <a class="gallery-link" href="${images[0].original}">
+    <img
+      class="gallery-image"
+      src="${images[0].preview}"
+      data-source="${images[0].original}"
+      alt="${images[0].description}"
+    />
+  </a>
+`;
+
+gallery.append(galleryItem);
